e507332038d0a80756b7455a5509536be55f9688
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-09-03 15:57:34 +0800

    Template Driven Forms - Angular
    Link: https://www.youtube.com/watch?v=CGLdH5ORX-Y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-temp-driv-forms';
+
+  onSubmit(f : NgForm){
+    console.log(f.value)
+  }
+
+  getValue(f : FormControl){
+    console.log(f);
+    
+  }
 }
